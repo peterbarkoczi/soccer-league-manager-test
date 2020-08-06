639becc3b23aef87df0dd63743aff6db0640da4c
peterbarkoczi <barkopetya@gmail.com>
2020-08-06 20:19:27 +0200

    add

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanagertest/SoccerLeagueManagerTestApplication.java
@@ -0,0 +1,13 @@
+package com.barkoczi.peter.soccerleaguemanagertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerLeagueManagerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerLeagueManagerTestApplication.class, args);
+    }
+
+}
