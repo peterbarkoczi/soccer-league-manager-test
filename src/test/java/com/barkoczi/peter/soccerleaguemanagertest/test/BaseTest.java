a18a619b4035177aea70440ee8d5344081ed1169
peterbarkoczi <barkopetya@gmail.com>
2020-08-06 20:15:46 +0200

    create BaseTest class

---
+++ src/test/java/com/barkoczi/peter/soccerleaguemanagertest/test/BaseTest.java
@@ -0,0 +1,35 @@
+package com.barkoczi.peter.soccerleaguemanagertest.test;
+
+import com.barkoczi.peter.soccerleaguemanagertest.pages.HomePage;
+import com.barkoczi.peter.soccerleaguemanagertest.pages.LeaguePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+    protected HomePage homePage;
+    protected LeaguePage leaguePage;
+
+    @BeforeAll
+    public static void setUpDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        this.driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
