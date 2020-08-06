95882d25c2a2e95254430c888138c8f523a42200
peterbarkoczi <barkopetya@gmail.com>
2020-08-06 20:20:32 +0200

    created HomePage

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanagertest/pages/HomePage.java
@@ -0,0 +1,76 @@
+package com.barkoczi.peter.soccerleaguemanagertest.pages;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Service
+public class HomePage extends BasePage {
+
+    @Autowired
+    private AddLocationModal addLocationModal;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "appTitle")
+    private WebElement appTitle;
+
+    @FindBy(id = "logInButton")
+    private WebElement logInButton;
+
+    @FindBy(id = "signInButton")
+    private WebElement signInButton;
+
+    @FindBy(id = "addLocationButton")
+    private WebElement addLocationButton;
+
+    @FindBy(id = "locationName")
+    private WebElement locationTitle;
+
+    @FindBy(id = "locationList")
+    private WebElement locationList;
+
+    @FindBy(className = "locationLink")
+    private List<WebElement> locationLinks;
+
+    public void navigateToHomePage() {
+        navigateTo("");
+    }
+
+    public void clickOnAddLocationButton() {
+        clickOn(addLocationButton);
+    }
+
+    public List<String> getNewLocationNames() {
+        List<WebElement> names = driver.findElements(By.className("locationLink"));
+        List<String> locationNames = new ArrayList<>();
+        for (WebElement name : names) {
+            locationNames.add(name.getText());
+        }
+        return locationNames;
+    }
+
+    public String createNewLocation(String locationName) {
+        clickOnAddLocationButton();
+        addLocationModal.fillAddNameField(locationName);
+        addLocationModal.clickOnSubmitButton();
+        return locationName;
+    }
+
+}
