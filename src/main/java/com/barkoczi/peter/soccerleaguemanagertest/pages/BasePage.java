e391fad96c9d82ec73091885a4ab66010a7b1ad2
peterbarkoczi <barkopetya@gmail.com>
2020-08-06 20:15:35 +0200

    create BasePage class

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanagertest/pages/BasePage.java
@@ -0,0 +1,40 @@
+package com.barkoczi.peter.soccerleaguemanagertest.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BasePage {
+
+    protected WebDriver driver;
+    String baseUrl;
+    WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 20), this);
+        this.baseUrl = "http://localhost:3000/";
+        this.wait = new WebDriverWait(driver, 20);
+    }
+
+    protected void navigateTo(String extendUrl) {
+        driver.get(baseUrl + extendUrl);
+    }
+
+    protected void clickOn(WebElement webElement) {
+        webElement.click();
+    }
+
+    protected void fillInputField(WebElement inputField, String input) {
+        inputField.sendKeys(input);
+    }
+}
