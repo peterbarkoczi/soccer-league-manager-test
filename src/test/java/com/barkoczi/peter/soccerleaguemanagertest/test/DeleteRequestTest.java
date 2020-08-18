a285a748f016b3ca8ccdf405078e49902747a481
peterbarkoczi <barkopetya@gmail.com>
2020-08-18 17:40:35 +0200

    ignore test

--- src/test/java/com/barkoczi/peter/soccerleaguemanagertest/test/DeleteRequestTest.java
+++ src/test/java/com/barkoczi/peter/soccerleaguemanagertest/test/DeleteRequestTest.java
@@ -1,21 +1,23 @@
-//package com.barkoczi.peter.soccerleaguemanagertest.test;
-//
-//import io.restassured.RestAssured;
-//import io.restassured.http.Method;
-//import io.restassured.response.Response;
-//import io.restassured.specification.RequestSpecification;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import static io.restassured.RestAssured.given;
-//
-//public class DeleteRequestTest {
-//
-//    @Test
-//    @DisplayName("Delete location test")
-//    public void deleteLocation() {
-//        String response = given().when().delete("http://localhost:8080/location/330").body().asString();
-//
-//        System.out.println(response);
-//    }
-//}
+package com.barkoczi.peter.soccerleaguemanagertest.test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Ignore;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteRequestTest {
+
+    @Ignore
+    @Test
+    @DisplayName("Delete location test")
+    public void deleteLocation() {
+        String response = given().when().delete("http://localhost:8080/location/330").body().asString();
+
+        System.out.println(response);
+    }
+}
