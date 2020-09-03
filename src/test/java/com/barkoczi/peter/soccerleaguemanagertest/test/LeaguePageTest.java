93d0a92a0359af171c6f08ca39b3e250966b391d
peterbarkoczi <barkopetya@gmail.com>
2020-09-03 11:44:03 +0200

    refactor: added new location for every test to check the operation

--- src/test/java/com/barkoczi/peter/soccerleaguemanagertest/test/LeaguePageTest.java
+++ src/test/java/com/barkoczi/peter/soccerleaguemanagertest/test/LeaguePageTest.java
@@ -1,10 +1,8 @@
 package com.barkoczi.peter.soccerleaguemanagertest.test;
 
-import com.barkoczi.peter.soccerleaguemanagertest.pages.AddLocationModal;
 import com.barkoczi.peter.soccerleaguemanagertest.pages.Header;
 import com.barkoczi.peter.soccerleaguemanagertest.pages.HomePage;
 import com.barkoczi.peter.soccerleaguemanagertest.pages.LeaguePage;
-import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,56 +19,53 @@ public class LeaguePageTest extends BaseTest {
         leaguePage = new LeaguePage(this.driver);
         header = new Header(this.driver);
         homePage.navigateToHomePage();
-
     }
 
-    @Ignore
-//    @Test
+    @Test
     @DisplayName("Header title is appeared")
     public void headerTitleIsAppearedTest() {
-        String locationName = createNewLocation("New test location");
-        homePage.clickOnLocation(locationName);
+        addNewLocation(testLocationName, driver);
+        homePage.clickOnLocation(testLocationName);
         assertTrue(leaguePage.locationHeaderTitleIsAppeared());
-    }
-
-    private String createNewLocation(String location) {
-        AddLocationModal addLocationModal = new AddLocationModal(this.driver);
-        homePage.clickOnAddLocationButton();
-        addLocationModal.fillAddNameField(location);
-        addLocationModal.clickOnSubmitButton();
-        return location;
+        deleteLocation(testLocationName);
     }
 
     @Test
     @DisplayName("Nav Menu is appeared")
     public void navMenuIsAppeared() {
-        String locationName = "New test location";
-        createNewLocation(locationName);
-        homePage.clickOnLocation(locationName);
+        addNewLocation(testLocationName, driver);
+        homePage.clickOnLocation(testLocationName);
         assertTrue(header.headerNavMenuIsAppeared());
+        deleteLocation(testLocationName);
     }
 
     @Test
-    @DisplayName("Is League title appeared")
+    @DisplayName("Is 'Bajnokság' appeared")
     public void checkLeagueTitle() {
-        homePage.clickOnLocation("Kistarcsa");
+        addNewLocation(testLocationName, driver);
+        homePage.clickOnLocation(testLocationName);
         header.clickOnLeaguesButton();
         assertTrue(leaguePage.isLeagueTitleAppeared());
+        deleteLocation(testLocationName);
     }
 
     @Test
     @DisplayName("Is Add League button appeared")
     public void checkAddLeagueButton() {
-        homePage.clickOnLocation("Kistarcsa");
+        addNewLocation(testLocationName, driver);
+        homePage.clickOnLocation(testLocationName);
         header.clickOnLeaguesButton();
         assertTrue(leaguePage.isAddLeagueButtonAppeared());
+        deleteLocation(testLocationName);
     }
 
     @Test
     @DisplayName("Is Leagues list appeared")
     public void checkLeaguesList() {
-        homePage.clickOnLocation("Kistarcsa");
+        addNewLocation(testLocationName, driver);
+        homePage.clickOnLocation(testLocationName);
         header.clickOnLeaguesButton();
         assertTrue(leaguePage.isLeaguesListAppeared());
+        deleteLocation(testLocationName);
     }
 }
